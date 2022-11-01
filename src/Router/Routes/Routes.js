1b26932ced793992e9e324ab99e93d477ac82f03
anowarzz <anowarhosen444@gmail.com>
2022-11-01 08:27:06 +0600

    hero sllider added

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,21 @@
+import { Children } from "react"
+import { createBrowserRouter } from "react-router-dom"
+import Main from "../../layout/Main"
+import Home from "../../Pages/Home/Home/Home"
+
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main />,
+        children: [
+            {
+                path:'/',
+                element: <Home />
+            }
+        ]
+    
+    }
+])
+
+  export default router
