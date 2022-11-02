94c9c8f8e66249b602d36eef7e50e7af0c1f4eb1
anowarzz <anowarhosen444@gmail.com>
2022-11-02 20:53:20 +0600

    banner responsive added

--- src/Pages/Home/Banner/BannerItem.js
+++ src/Pages/Home/Banner/BannerItem.js
@@ -13,22 +13,24 @@ const {image, id, prev, next} = slide
         <img src={image} alt="img1" className="w-full rounded-xl" />
         </div>
         
-            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
-                <h1 className=' text-6xl font-bold text-white'>Affordable <br />
+            <div className='flex flex-col'>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 md:left-24 top-20 md:top-1/4">
+                <h1 className='text-2xl md:text-4xl lg:text-6xl font-bold text-white'>Affordable <br />
                 Price for Car <br />
                 Servicing
                 </h1>
             </div>
         
-            <div className="absolute w-2/5 flex justify-end transform -translate-y-1/2 left-24 top-1/2">
-               <p className='text-white text-lg md:text-xl'>
+            <div className="absolute w-3/5 lg:w-2/5 flex  justify-end transform -translate-y-1/2 right-0 md:left-24 top-1/3 md:top-1/2">
+               <p className='text-white text-lg md:text-xl ml-4'>
                There are many variations of passages of  available, but the majority have suffered alteration in some form
                </p>
             </div>
+            </div>
         
-            <div className="absolute w-2/5 flex justify-start transform -translate-y-1/2 left-24 top-3/4">
-            <button className="btn hover:bg-[#ff3911a1] bg-brightRed mr-5">Discover More</button>
-            <button className="btn  btn-outline border-brightRed hover:bg-brightRed "><p className='text-white'>Latest Project</p></button>
+            <div className="absolute w-2/5 flex flex-col gap-2 md:flex-row justify-start transform -translate-y-1/2 left-10 md:left-24 top-3/4">
+            <button className="btn  w-4/5 md:w-auto hover:bg-[#ff3911a1] bg-brightRed mr-5">Discover More</button>
+            <button className="btn w-4/5 md:w-auto  btn-outline border-brightRed hover:bg-brightRed "><p className='text-white'>Latest Project</p></button>
             </div>
         
             <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
