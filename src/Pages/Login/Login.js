b37207bd8ee8c7e23f833f8082ed8f45e40a6200
anowarzz <anowarhosen444@gmail.com>
2022-11-02 22:59:16 +0600

    firebase added and encrypted via env

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
 
 const Login = () => {
+
+
+const handleLogin = (event) => {
+    event.preventDefault();
+
+
+}
+
+
   return (
     <div className="hero w-full my-20">
       <div className="hero-content grid md:grid-cols-2 gap-20 flex-col lg:flex-row">
@@ -11,7 +20,7 @@ const Login = () => {
         </div>
         <div className="card  w-full border border-gray-200 shadow-md">
           <h1 className="text-3xl md:text-5xl text-center font-bold">Login</h1>
-          <form className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-bold">Email</span>
@@ -32,15 +41,17 @@ const Login = () => {
                 className="input input-bordered"
               />
             </div>
-            <label className="label text-center">
+            <div className="text-end my-5">
+            <label className="">
               <Link
-                href="/"
+                to="/"
                 alt=""
                 className="label-text-alt link link-hover text-center"
               >
                 Forgot password?
               </Link>
             </label>
+            </div>
             <div className="form-control mt-6">
               <input
                 className="btn bg-brightRed border-none"
